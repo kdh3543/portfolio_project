601178ffc19da62d509090893cf5f4ac60b4917b
kdh3543 <rlaehdgus3543@naver.com>
2023-03-20 19:03:35 +0900

    Fix: mainpage 수정

--- src/pages/mainpage/index.ts
+++ src/pages/mainpage/index.ts
@@ -8,7 +8,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   // const API_URL = "https://api.themoviedb.org/3";
 
   const result = await axios.get(
-    `${CONFIG.API_URL}/movie/popular?api_key=${CONFIG.API_KEY}&language=ko-KR&sort_by=popularity.desc&release_date.gte=2023-01-01&release_date.lte=2023-03-20&page=1&adults=false`
+    `${CONFIG.API_URL}/movie/popular?api_key=${CONFIG.API_KEY}&language=ko-KR&sort_by=popularity.desc&release_date.gte=2022-01-01&release_date.lte=2022-03-20&page=1&include_adult=false`
   );
 
   const data = result.data;
